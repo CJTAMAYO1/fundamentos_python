858fea9914117d42160e269dabc5765f851e3de0
carlo tamayo <cejota708@gmail.com>
2023-06-08 14:32:53 -0600

    02 Variables

---
+++ variables.py
@@ -0,0 +1,14 @@
+#python es NO tipado
+name = "El sergio"
+age = 39
+is_single = False 
+print(name)
+print(age)
+print(is_single)
+
+"""
+Hola soy Carlo, tengo 19 años y soltero = True
+"""
+#utilizando format string
+print("Hola soy {}, tengo {} años y soltero = {}". format (name, age, is_single))
+print(f"Hola soy {name}, tengo {age} años y soltero = {is_single}")
