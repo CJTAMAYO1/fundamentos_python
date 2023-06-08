c570101d94f1c9e15e56463ca15db9bd83e206bf
carlo tamayo <cejota708@gmail.com>
2023-06-08 14:14:48 -0600

    Inicio con python

---
+++ main.py
@@ -0,0 +1,12 @@
+# funcion print ()
+print("Este es un string","Hola adrian")
+print ("este es un entero", 35)
+print ("Este es un float", 3.14)
+print("Este es un booleano", True)
+
+# fuction type nos permite saber tipos de datos 
+
+print(type(True))
+print(type(3.14))
+print(type(3))
+print(type(""))
