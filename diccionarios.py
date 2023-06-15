e3d1c336b645448341fb0d48123327e57e31b837
carlo tamayo <cejota708@gmail.com>
2023-06-15 13:39:48 -0600

    10 Diccionarios

---
+++ diccionarios.py
@@ -0,0 +1,19 @@
+#diccionarios
+#{"key": "Value", "Key":"Value"}
+
+team= {
+    "name": "City",
+    "Country": "Englad",
+    "Champions":1,
+    "Players": ['haland', 'grealinsh']
+}
+    
+
+print(type(team))
+print(team)
+print(team["name"])
+print(team["playes"])
+team ["players"].append("Kevin")
+team ["name"]="Manchester"
+team ["league"]="Premier league"
+print(team)
